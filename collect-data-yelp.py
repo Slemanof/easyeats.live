ae03fdfa1e4ec0e9e0c2ea491cd9d937df30aca4
ilyassakhanov <ilyas.sakhanov@gmail.com>
2020-03-03 14:31:23 +0100

    makes the code a little less messy

--- collect-data-yelp.py
+++ collect-data-yelp.py
@@ -6,11 +6,12 @@ response =urllib.request.urlopen('https://www.yelp.cz/search?find_desc=&find_loc
 soup = BeautifulSoup(response, 'html.parser')
 restaurant_names_html = soup.find_all(class_='lemon--a__373c0__IEZFH link__373c0__1G70M link-color--inherit__373c0__3dzpk link-size--inherit__373c0__1VFlE')
 restaurant_addresses_html = soup.find_all(class_ ="lemon--span__373c0__3997G raw__373c0__3rcx7")
-restaurant_rating_html = soup.find_all(class_ = "lemon--span__373c0__3997G text__373c0__2Kxyz priceRange__373c0__2DY87 text-color--black-extra-light__373c0__2OyzO text-align--left__373c0__2XGa- text-bullet--after__373c0__3fS1Z")
+restaurant_rating_html = soup.find_all(class_ = 'lemon--span__373c0__3997G text__373c0__2Kxyz priceRange__373c0__2DY87 text-color--black-extra-light__373c0__2OyzO text-align--left__373c0__2XGa- text-bullet--after__373c0__3fS1Z')
 restaurant_name = re.findall(r"name=\".*?\"", str(restaurant_names_html))
 restaurant_star= re.findall(r'\d\.?\d? hvězdičkové hodnocení', str(soup))
 restaurant_address = re.findall(r'3rcx7">.*?<',str(restaurant_addresses_html))
 restaurant_address = restaurant_address[1:]
 restaurant_price = re.findall(r'3fS1Z">.*?</span>', str(restaurant_rating_html))
+
 for possition in range(len(restaurant_star)):
     print(restaurant_address[possition][7:-1] + " " + restaurant_name[possition][6:-1]+ " "+ (restaurant_star[possition])[:-22] + " " + restaurant_price[possition][7:-7])
