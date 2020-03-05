53cb27e348ef757da4e9753389f7537529292340
ilyassakhanov <ilyas.sakhanov@gmail.com>
2020-03-05 19:04:32 +0100

    adds a functionality to collect price and put it into a dataset file

--- collect-data-yelp.py
+++ collect-data-yelp.py
@@ -10,9 +10,11 @@ restaurants__html = soup.find_all(
     class_='lemon--div__373c0__1mboc largerScrollablePhotos__373c0__3FEIJ arrange__373c0__2C9bH border-color--default__373c0__3-ifU')
 restaurant_type_pattern = r"lemon--a__373c0__IEZFH link__373c0__1G70M link-color--inherit__373c0__3dzpk link-size--default__373c0__7tls6\" href=\".*?\" rel=\"\" role=\".*?\">.*?<"
 restaurant_name_link_pattern = r"lemon--a__373c0__IEZFH link__373c0__1G70M link-color--inherit__373c0__3dzpk link-size--inherit__373c0__1VFlE\" href=\".*?\" name=\".*?\""
+restaurant_price_pattern = r"\"lemon--span__373c0__3997G text__373c0__2Kxyz priceRange__373c0__2DY87 text-color--black-extra-light__373c0__2OyzO text-align--left__373c0__2XGa- text-bullet--after__373c0__3fS1Z\">.*?<"
 restaurant_name_pattern = r'name=\".*?\"'
+restaurant_price_pattern = r"\"lemon--span__373c0__3997G text__373c0__2Kxyz priceRange__373c0__2DY87 text-color--black-extra-light__373c0__2OyzO text-align--left__373c0__2XGa- text-bullet--after__373c0__3fS1Z\">.*?<"
 with open('/home/Basurman/restaurant_recommender_2001/restaurant_dataset_yelp.csv', mode='w') as csv_file: #A path should be modified according to your file location
-    fieldnames = ['rest_name', 'rest_star','vegetarian']
+    fieldnames = ['rest_name', 'rest_star','vegetarian', 'price']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     for restaurant in restaurants__html:
         if(['target="">Památky<'] in [re.findall(r"target=\"\">.*?<", name) for name in ((re.findall(restaurant_type_pattern, str(restaurant))))]):
@@ -25,4 +27,4 @@ with open('/home/Basurman/restaurant_recommender_2001/restaurant_dataset_yelp.cs
                 restaurant_name_link_pattern, str(restaurant))[0]
             writer.writeheader()
             writer.writerow(
-                {'rest_name': ((re.findall(restaurant_name_pattern, name_link))[0])[6:-1], 'rest_star': ((re.findall(r'\d\.?\d? hvězdičkové hodnocení', str(restaurant)))[0])[:-22], 'vegetarian':veg})
+                {'rest_name': ((re.findall(restaurant_name_pattern, name_link))[0])[6:-1], 'rest_star': ((re.findall(r'\d\.?\d? hvězdičkové hodnocení', str(restaurant)))[0])[:-22], 'vegetarian':veg, 'price':((re.findall(restaurant_price_pattern,str(restaurant)))[0])[180:-1]})
