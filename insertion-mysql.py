947aa7032476d18b14f383fd26ef15ccdae9cd8b
ilyassakhanov <ilyas.sakhanov@gmail.com>
2020-03-28 13:36:57 +0100

    adds a functionality to add multiple cuisines to the restaurant

--- insertion-mysql.py
+++ insertion-mysql.py
@@ -1,23 +1,6 @@
 import mysql.connector
 
 
-def insert(res_id, res_name, res_address, res_rating, res_cost, res_cuisine):
-    cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
-                                  host='127.0.0.1',
-                                  database='restaurant_recommender')
-    cursor = cnx.cursor()
-    add_restaurant = ("INSERT INTO restaurant (id, name, address, rating, cost_for_2) VALUES (" +
-                      str(res_id) + ", \""+res_name+"\", \""+res_address+"\", \""+str(res_rating)+"\", \""+str(res_cost)+"\")")
-    cursor.execute(add_restaurant)
-    cnx.commit()
-    cursor.close()
-    cnx.close()
-    return
-
-
-# insert(4, 'Test4', "Na pankraci", 4.5, 400 , "Czech")
-
-
 def check_cuisines(cuisine):
     cuisine_exisits = False
     cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
@@ -50,11 +33,27 @@ def cuisines(res_id, res_cuisine):
     for cuisine in cuisines_list:
         check_cuisines(cuisine)
         add_restaurant_cuisine = ("INSERT INTO restaurant_cuisine (restaurant_id, cuisine_id) VALUES ("+str(
-            res_id)+",(SELECT id FROM cuisines WHERE name = \""+res_cuisine+"\"))")
+            res_id)+",(SELECT id FROM cuisines WHERE name = \""+cuisine+"\"))")
         cursor.execute(add_restaurant_cuisine)
-        cnx.commit()
-        cursor.close()
-        cnx.close()
+    cnx.commit()
+    cursor.close()
+    cnx.close()
     return
 
 # cuisines(1, "Czech")
+
+def insert(res_id, res_name, res_address, res_rating, res_cost, res_cuisine):
+    cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
+                                  host='127.0.0.1',
+                                  database='restaurant_recommender')
+    cursor = cnx.cursor()
+    add_restaurant = ("INSERT INTO restaurant (id, name, address, rating, cost_for_2) VALUES (" +
+                      str(res_id) + ", \""+res_name+"\", \""+res_address+"\", \""+str(res_rating)+"\", \""+str(res_cost)+"\")")
+    cursor.execute(add_restaurant)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    cuisines(res_id, res_cuisine)
+    return
+
+insert(6, 'Test6', "Na pankraci", 4.5, 400 , "Czech, Bulgar")
