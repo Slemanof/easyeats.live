fc513add39f0b1a390b3ac83bb1e0e30a9c76371
ilyassakhanov <ilyas.sakhanov@gmail.com>
2020-03-28 21:54:59 +0100

    completes a function to add data to the database

--- insertion-mysql.py
+++ insertion-mysql.py
@@ -14,14 +14,12 @@ def check_cuisines(cuisine):
             cuisine_exisits = True
             return
     if cuisine_exisits == False:
-        cursor.execute("INSERT INTO cuisines (name) VALUES (\"" + cuisine+"\")")
+        cursor.execute(
+            "INSERT INTO cuisines (name) VALUES (\"" + cuisine+"\")")
     cnx.commit()
     cursor.close()
     cnx.close()
     return
-
-
-check_cuisines("Czech")
 
 
 def cuisines(res_id, res_cuisine):
@@ -40,15 +38,14 @@ def cuisines(res_id, res_cuisine):
     cnx.close()
     return
 
-# cuisines(1, "Czech")
 
-def insert(res_id, res_name, res_address, res_rating, res_cost, res_cuisine):
+def insert(res_id, res_name, res_address, res_rating, res_cost, res_cuisine, res_image, res_vegan, res_vegetatian, res_credit_card, res_gluten_free, res_takeaway, res_phone1, res_phone2, res_menu):
     cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
                                   host='127.0.0.1',
                                   database='restaurant_recommender')
     cursor = cnx.cursor()
-    add_restaurant = ("INSERT INTO restaurant (id, name, address, rating, cost_for_2) VALUES (" +
-                      str(res_id) + ", \""+res_name+"\", \""+res_address+"\", \""+str(res_rating)+"\", \""+str(res_cost)+"\")")
+    add_restaurant = ("INSERT INTO restaurant (id, name, address, rating, cost_for_2 , image, vegan, vegetarian, credit_card, gluten_free, takeaway, phone_num1, phone_num2, menu) VALUES (" +
+                      str(res_id) + ", \""+res_name+"\", \""+res_address+"\", \""+str(res_rating)+"\", \""+str(res_cost)+"\",\""+res_image+"\", \""+str(res_vegan)+"\" , \""+str(res_vegetatian)+"\", \""+str(res_credit_card)+"\" , \""+str(res_gluten_free)+"\", \""+str(res_takeaway)+"\" , \""+str(res_phone1)+"\", \""+str(res_phone2)+"\", \'"+res_menu+"\')")
     cursor.execute(add_restaurant)
     cnx.commit()
     cursor.close()
@@ -56,4 +53,6 @@ def insert(res_id, res_name, res_address, res_rating, res_cost, res_cuisine):
     cuisines(res_id, res_cuisine)
     return
 
-insert(6, 'Test6', "Na pankraci", 4.5, 400 , "Czech, Bulgar")
+
+insert(21, 'Test21', "Na pankraci", 4.5, 400,
+       "Czech, Bulgar, German", "foodlink", 1, 0, 0,0, 0, "880055", 0, "{\"status\": \"graduated\",\"DOB\": \"1968-08-31\",\"enroll_semestr\": \"1998SN\",\"nationality\": \"American\"}")
