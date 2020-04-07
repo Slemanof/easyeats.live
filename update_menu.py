289ca8c3f65a8d71b276903a4897b28e7c47d64c
ilyassakhanov <ilyas.sakhanov@gmail.com>
2020-04-07 21:26:39 +0200

    adds a script that updates daily menu

---
+++ update_menu.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from zomato_api import get_menu
+
+def update_menu_database(res_id, menu):
+    cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
+                                  host='127.0.0.1',
+                                  database='restaurant_recommender')
+    cursor = cnx.cursor()
+    update_sql = ("UPDATE restaurant SET  menu =\'"+ menu +"\'  WHERE id = "+str(res_id))
+    cursor.execute(update_sql)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+def update_main():
+    cnx = mysql.connector.connect(user='script', password='LetMeIn:)123',
+                                  host='127.0.0.1',
+                                  database='restaurant_recommender')
+    cursor = cnx.cursor()
+    get_ids= ("SELECT id FROM restaurant")
+    cursor.execute(get_ids)
+    for row in cursor:
+        update_menu_database((row[0]), (get_menu(str(row[0]))))
+    cursor.close()
+    cnx.close()
+
+update_main()
