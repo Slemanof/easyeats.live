1e05d13f515da65c6052b6283bc4236d59aeddce
riccardosl <riccardostoppani@gmail.com>
2020-04-07 21:15:56 +0200

    update json function

--- zomato-api.py
+++ zomato-api.py
@@ -3,8 +3,11 @@ import geopy.distance
 import json
 import insertionMysql
 
-zomato_api = '6bc71cad65004dd66dbb46d16667630a'
 
+with open('api.json') as creds:
+    credentials = json.load(creds)
+
+zomato_api = credentials['zomato']
 
 user_coordinates = []
 
