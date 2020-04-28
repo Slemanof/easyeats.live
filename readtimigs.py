f34ff630a58ec54fb06ce6b6969b7ce2b2518cd0
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-04-28 20:26:04 +0200

    Modify the script to work on a wider range of menus from Zomato API

--- readtimigs.py
+++ readtimigs.py
@@ -10,15 +10,14 @@ def findtime(time):
     return result[:-1]
 
 
-week_dict = {"Mon": 0, "Tue": 1, "Wed": 2,
-             "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
+week_dict = {"Mon": 0, "Po": 0, "Tue": 1, "Út": 1, "Wed": 2, "St": 3,
+             "Thu": 3, "Čt": 4, "Fri": 4, "Pá": 4, "Sat": 5, "So": 5, "Sun": 6, "Ne": 6}
 week_lst = ["Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday", "Sunday"]
 
 
 def put_timings_order(row, days, hrs_dict):
-    days = days.replace(")", "")
-    for day in days.split(", "):
+    for day in days:
         hrs_dict[week_lst[week_dict[day]]] = findtime(row)
     return hrs_dict
 
@@ -37,21 +36,33 @@ def put_timings_standalone(row, day, hrs_dict):
     hrs_dict[week_lst[week_dict[day]]] = findtime(row)
     return hrs_dict
 
+def satitize_zomato_timings(zomato_timing):
+    zomato_timing = zomato_timing.replace(" AM", ":00").replace(" PM", ":00").replace("to", "").replace("12 Noon", "12:00").replace("12 Midnight", "24:00")
+
+    return zomato_timing
+    
 
 def get_opening_hrs(zomato_timing):
-    opening_hrs_dict = {}
-    for row in zomato_timing.split("),"):
-        order_days = re.findall(r"[D-Z][a-z]{2}, [D-Z][a-z]{2}", row)
-        interval_days = re.findall(
-            r"[D-Z][a-z]{2}-[D-Z][a-z]{2}", row)
-        standalone_days = re.findall(r"[D-Z][a-z]{2}", row)
-        if order_days:
-            opening_hrs_dict = put_timings_order(
-                row, order_days[0], opening_hrs_dict)
-        if interval_days:
-            opening_hrs_dict = put_timings_interval(
-                row, interval_days[0], opening_hrs_dict)
-        if standalone_days:
-            opening_hrs_dict = put_timings_standalone(
-                row, standalone_days[0], opening_hrs_dict)
-    return str(opening_hrs_dict)
+    try:
+        zomato_timing = satitize_zomato_timings(zomato_timing)
+        opening_hrs_dict = {}
+        for row in zomato_timing.split("),"):
+            order_days = re.findall(r"[D-ZÚ]\w{1,2}(?=[, )])", row)
+            interval_days = re.findall(
+                r"[D-ZÚ]\w{1,2}-[D-ZÚ]\w{1,2}", row)
+            standalone_days = re.findall(r"[D-ZÚ]\w{2}", row)
+            if order_days:
+                opening_hrs_dict = put_timings_order(
+                    row, order_days, opening_hrs_dict)
+            if interval_days:
+                opening_hrs_dict = put_timings_interval(
+                    row, interval_days[0], opening_hrs_dict)
+            if standalone_days:
+                opening_hrs_dict = put_timings_standalone(
+                    row, standalone_days[0], opening_hrs_dict)
+        return str(opening_hrs_dict)
+    except:
+        return "Unavaliable"
+
+
+print(get_opening_hrs("11 AM - 11 PM (Po-Pá), 12 Noon - 11 PM (So-Ne)"))
