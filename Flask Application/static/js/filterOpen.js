75d11c7db145f3020cacabada9efc2b857dd7949
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-05-05 21:55:07 +0200

    Cleaning the code

--- Flask Application/static/js/filterOpen.js
+++ Flask Application/static/js/filterOpen.js
@@ -1,34 +1,25 @@
-const Filtershow = {
-  toggleClass(element, elementClass) {
-    element.classList.toggle(elementClass);
-  },
-  addClass(element, elementClass) {
-    element.classList.toggle(elementClass);
-  },
-  removeClass(element, elementClass) {
-    element.classList.toggle(elementClass);
-  },
-
-  showMore(element, exccerpt) {
-    element.addEventListener('click', (event) => {
-      const linkText = event.target.textContent;
-      event.preventDefault();
-      if (linkText == 'Show more') {
-        element.textContent = 'Show less';
-        this.toggleClass(
-          document.querySelector('.main__filter__body-hidden'),
-          'main__filter__body-hidden--visable'
-        );
-      } else {
-        element.textContent = 'Show more';
-        this.toggleClass(
-          document.querySelector('.main__filter__body-hidden '),
-          'main__filter__body-hidden--visable'
-        );
-      }
-    });
-  },
-};
-Filtershow.showMore(
-  document.querySelector('.main__filter__body-show-more-btn')
+const showMoreFilter = document.querySelector(
+  '.main__filter__body-show-more-btn'
 );
+
+function toggleClass(element, elementClass) {
+  element.classList.toggle(elementClass);
+}
+
+showMoreFilter.addEventListener('click', showMore);
+
+function showMore() {
+  if (showMoreFilter.textContent == 'Show more') {
+    showMoreFilter.textContent = 'Show less';
+    toggleClass(
+      document.querySelector('.main__filter__body-hidden'),
+      'main__filter__body-hidden--visable'
+    );
+  } else {
+    showMoreFilter.textContent = 'Show more';
+    toggleClass(
+      document.querySelector('.main__filter__body-hidden '),
+      'main__filter__body-hidden--visable'
+    );
+  }
+}
