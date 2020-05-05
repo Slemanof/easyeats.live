bc3c3d5485fc99e087379fbf01114828e8a4cb2e
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-05 15:39:44 +0200

    Add a script that inserts user data and also encrypts/hashes sensible information about that user

---
+++ insertUser.py
@@ -0,0 +1,41 @@
+import mysql.connector
+import bcrypt
+import json
+import os
+import base64
+from cryptography.fernet import Fernet
+
+with open('api.json') as creds:
+    credentials = json.load(creds)
+
+
+cnx = mysql.connector.connect(user=credentials['mysql_user'], password=credentials['mysql_password'],
+                              host='127.0.0.1',
+                              database=credentials['mysql_database_name'])
+cursor = cnx.cursor()
+
+
+def hash_smth(secret):
+    secret = secret.encode()
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(secret, salt)
+    return hashed.decode('utf-8')
+
+
+def encrypt_smth(secret):
+    key_input = credentials['encryption_key']
+    key = base64.urlsafe_b64encode(key_input.encode())
+    cipher_suite = Fernet(key)
+    ciphered_text = cipher_suite.encrypt(secret.encode())
+    return ciphered_text.decode('utf-8')
+
+
+def add_main(email, name, password):
+    add_user = (
+        "INSERT INTO user (email, name, password) VALUES (\" " + encrypt_smth(email)+"\", \"" + name+"\", \"" + hash_smth(password)+"\")")
+    cursor.execute(add_user)
+    cnx.commit()
+    return
+
+
+cnx.close()
