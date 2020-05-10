f425002cbba695acdbe7895467e9f27dd6ece0d4
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-05-10 06:43:29 +0200

    Code optimization
    The filtering procedures have been split into function which are created outside of the route function. Thus, filtering processes by cuisines, distance and dietary options are performed by 3 separate function respectively, which are called by each other in the route function. This has been done in order to not overload the route function and to structure the code

--- Flask Application/app.py
+++ Flask Application/app.py
@@ -12,43 +12,76 @@ app.config['MYSQL_DB'] = 'restApp'
 
 mysql = MySQL(app)
 
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     cur = mysql.connection.cursor()
-    result = request.args.to_dict(flat=False)
+    chosen_filters = request.args.to_dict(flat=False)
 
-    fields = """id, name, address, image, rating, price_range, timing,
-                                            vegan, vegetarian, gluten_free, credit_card, takeaway,
-                                            phone_num1, phone_num2, usual_menu_url, menu, latitute,
-                                            longitude"""
+    cuisines_option_query = cuisines_option(chosen_filters)
 
-    cuisine_query = """SELECT r.id, r.name, r.address, r.image, r.rating, r.price_range, r.timing,
-                                                r.vegan, r.vegetarian, r.gluten_free, r.credit_card, r.takeaway,
-                                                r.phone_num1, r.phone_num2, r.usual_menu_url, r.menu,
-                                                r.latitute, r.longitude, GROUP_CONCAT(c.name separator ', ') as cuisines
-                                                FROM restaurant r LEFT JOIN
-                                                restaurant_cuisine rc
-                                                ON rc.restaurant_id = r.id LEFT JOIN
-                                                cuisines c
-                                                ON c.id = rc.cuisine_id"""
+    location_option_query = location_option(cuisines_option_query, chosen_filters)
 
-    if 'cuisines' in result:
-        cuisines = tuple(result.get('cuisines'))
+    cur.execute(additional_options(location_option_query, chosen_filters))
+    data = cur.fetchall()
+
+    new_data = []
+    for i in data:
+        new_data.append(list(i))
+
+    for i in new_data:
+        i[6] = json.loads(i[6])
+        i[15] = json.loads(i[15])
+
+    return render_template('index.html', data=new_data)
+
+
+def additional_options(query, filters):
+    additional_filters = ['vegetarian', 'vegan', 'gluten_free', 'credit_card', 'takeaway']
+    for filter_name in additional_filters:
+        if filter_name in filters:
+            query = "SELECT * FROM (%s) as t WHERE %s=1 " % (query, filter_name)
+        else:
+            query = "SELECT * FROM (%s) as t" % query
+
+    return query
+
+
+def cuisines_option(filters):
+    query_fragment = """SELECT r.id, r.name, r.address, r.image, r.rating, r.price_range, r.timing,
+                        r.vegan, r.vegetarian, r.gluten_free, r.credit_card, r.takeaway,
+                        r.phone_num1, r.phone_num2, r.usual_menu_url, r.menu,
+                        r.latitute, r.longitude, GROUP_CONCAT(c.name separator ', ') as cuisines
+                        FROM restaurant r LEFT JOIN
+                        restaurant_cuisine rc
+                        ON rc.restaurant_id = r.id LEFT JOIN
+                        cuisines c
+                        ON c.id = rc.cuisine_id"""
+
+    if 'cuisines' in filters:
+        cuisines = tuple(filters.get('cuisines'))
 
         if len(cuisines) == 1:
-            filter_by_cuisine = ("%s WHERE c.name = '%s' GROUP BY r.id " % (cuisine_query, cuisines[0]))
+            filtered_by_cuisine = ("%s WHERE c.name = '%s' GROUP BY r.id " % (query_fragment, cuisines[0]))
 
         else:
-            filter_by_cuisine = ("%s WHERE c.name = '%s' GROUP BY r.id " % (cuisine_query, (cuisines,)))
+            filtered_by_cuisine = ("%s WHERE c.name = '%s' GROUP BY r.id " % (query_fragment, (cuisines,)))
 
     else:
-        filter_by_cuisine = ("%s GROUP BY r.id " % cuisine_query)
+        filtered_by_cuisine = ("%s GROUP BY r.id " % query_fragment)
 
-    if 'lat' and 'lon' in result:
+    return filtered_by_cuisine
 
-        lat = result.get('lat')[0]
-        lon = result.get('lon')[0]
+
+def location_option(cuisine_query, filters):
+    query_fragment = """id, name, address, image, rating, price_range, timing,
+                vegan, vegetarian, gluten_free, credit_card, takeaway,
+                phone_num1, phone_num2, usual_menu_url, menu, latitute,
+                longitude"""
+
+    if 'lat' and 'lon' in filters:
+
+        lat = filters.get('lat')[0]
+        lon = filters.get('lon')[0]
 
         formula = ("""(6371 * acos(cos( radians(%s
                 )) * cos( radians( latitute )) * cos( radians( longitude ) - radians(%s
@@ -59,26 +92,12 @@ def home():
                    FORMAT(%s, 2) AS distance,
                    FORMAT((%s * 12), 1) AS time
                    FROM (%s) as t HAVING distance < 3 ORDER
-                   BY distance""" % (fields, formula, formula, filter_by_cuisine))
+                   BY distance""" % (query_fragment, formula, formula, cuisine_query))
 
     else:
-        filter_by_distance = ("SELECT * FROM (%s) as t" % filter_by_cuisine)
+        filter_by_distance = ("SELECT * FROM (%s) as t" % cuisine_query)
 
-        def additional_options(query):
-        additional_filters = ['vegetarian', 'vegan', 'gluten_free', 'credit_card', 'takeaway']
-        for filter_name in additional_filters:
-            if filter_name in result:
-                query = "SELECT * FROM (%s) as t WHERE %s=1 " % (query, filter_name)
-            else:
-                query = "SELECT * FROM (%s) as t" % query
-
-        return query
-
-    cur.execute(additional_options(filter_by_distance))
-    data = cur.fetchall()
-
-    return render_template('index.html', data=data)
-
+    return filter_by_distance
 
 if __name__ == "__main__":
     app.run(debug=True)
