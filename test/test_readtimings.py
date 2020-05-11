c4f6a6bc63b8c4dfc77d90366c6d8647646fc895
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-11 23:42:07 +0200

    Change the error messafe got tests to contain names of the finctions instead of desciption of that function, add a test for put_timings_standalone

--- test/test_readtimings.py
+++ test/test_readtimings.py
@@ -10,13 +10,17 @@ def test_findtime():
 
 def test_put_timings_order():
     assert readtimigs.put_timings_order('11:30 to 01:00 (Mon, Tue, Wed, Thu, Sun)', ['Mon', 'Tue', 'Wed', 'Thu', 'Sun'], {}) == {
-        'Monday': '11:30-01:00', 'Tuesday': '11:30-01:00', 'Wednesday': '11:30-01:00', 'Thursday': '11:30-01:00', 'Sunday': '11:30-01:00'}, 'Function for putting time separated by coma is bad'
+        'Monday': '11:30-01:00', 'Tuesday': '11:30-01:00', 'Wednesday': '11:30-01:00', 'Thursday': '11:30-01:00', 'Sunday': '11:30-01:00'}, 'put_timings_order is bad'
     assert readtimigs.put_timings_order('1:00 - 11:00 (Po, Pá)', ['Po', 'Pá'], {}) == {
-        'Monday': '1:00-11:00', 'Friday': '1:00-11:00'}, 'Function for putting time separated by coma is bad'
+        'Monday': '1:00-11:00', 'Friday': '1:00-11:00'}, 'put_timings_order is bad'
 
 
 def test_put_timings_interval():
     assert readtimigs.put_timings_interval('11:30 AM to 11:30 PM (Mon-Fri)', 'Mon-Fri', {}) == {
-        'Monday': '11:30-11:30', 'Tuesday': '11:30-11:30', 'Wednesday': '11:30-11:30', 'Thursday': '11:30-11:30', 'Friday': '11:30-11:30'}
+        'Monday': '11:30-11:30', 'Tuesday': '11:30-11:30', 'Wednesday': '11:30-11:30', 'Thursday': '11:30-11:30', 'Friday': '11:30-11:30'}, 'put_timings_interval is bad'
     assert readtimigs.put_timings_interval('11:00 - 11:00 (Po-Pá)', 'Po-Pá', {}) == {
-        'Monday': '11:00-11:00', 'Tuesday': '11:00-11:00', 'Wednesday': '11:00-11:00', 'Thursday': '11:00-11:00', 'Friday': '11:00-11:00'}
+        'Monday': '11:00-11:00', 'Tuesday': '11:00-11:00', 'Wednesday': '11:00-11:00', 'Thursday': '11:00-11:00', 'Friday': '11:00-11:00'}, 'put_timings_interval is bad'
+
+
+def test_put_timings_standalone():
+    assert readtimigs.put_timings_standalone('11:00 - 11:00 (Mon)', 'Mon', {}) == {'Monday': '11:00-11:00'}
