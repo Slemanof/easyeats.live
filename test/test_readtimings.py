3f3c4a2fc9c24e2f7f6f57a1d710304f2e6e1626
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-11 19:24:00 +0200

    Add one more asser for a fuction that puts opening hours for input separated by comma

--- test/test_readtimings.py
+++ test/test_readtimings.py
@@ -4,9 +4,12 @@ import readtimigs
 def test_findtime():
     assert readtimigs.findtime(
         "12:00 - 18:00") == "12:00-18:00", "Time conversion method is bad"
-    assert readtimigs.findtime("16:00 - 1:00") == "16:00-1:00", "Time conversion method is bad"
+    assert readtimigs.findtime(
+        "16:00 - 1:00") == "16:00-1:00", "Time conversion method is bad"
 
 
 def test_put_timings_order():
     assert readtimigs.put_timings_order('11:30 to 01:00 (Mon, Tue, Wed, Thu, Sun)', ['Mon', 'Tue', 'Wed', 'Thu', 'Sun'], {}) == {
-        'Monday': '11:30-01:00', 'Tuesday': '11:30-01:00', 'Wednesday': '11:30-01:00', 'Thursday': '11:30-01:00', 'Sunday': '11:30-01:00'}
+        'Monday': '11:30-01:00', 'Tuesday': '11:30-01:00', 'Wednesday': '11:30-01:00', 'Thursday': '11:30-01:00', 'Sunday': '11:30-01:00'}, 'Function for putting time separated by coma is bad'
+    assert readtimigs.put_timings_order('11:00 - 11:00 (Po, Pá)', ['Po', 'Pá'], {}) == {
+        'Monday': '11:00-11:00', 'Friday': '11:00-11:00'}, 'Function for putting time separated by coma is bad'
