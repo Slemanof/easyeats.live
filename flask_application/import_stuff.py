ab9fa6bbd456a647e866bb788b52d0fa9e68ffd1
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-05-29 23:09:32 +0200

    Update import_stuff.py

--- flask_application/import_stuff.py
+++ flask_application/import_stuff.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_keys():
-    with open('../api.json') as creds:
+    with open('api.json') as creds:
         credentials = json.load(creds)
     key = credentials['encryption_key']
     return key
